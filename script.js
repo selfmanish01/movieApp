bf3b0aea0955515aa0f15634c9925197a81ed883
MANISH KUMAR <103636288+selfmanish01@users.noreply.github.com>
2023-08-20 18:23:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+      window.addEventListener('scroll', function() {
+        var welcomeText = document.querySelector('.welcome-text');
+        var scrollPosition = window.scrollY;
+        var windowHeight = window.innerHeight;
+        var elementPosition = welcomeText.getBoundingClientRect().top;
+      
+        if (elementPosition - windowHeight <= -100) {
+          welcomeText.classList.add('hidden');
+        } else {
+          welcomeText.classList.remove('hidden');
+        }
+      });
